1ac4b6f51a1fd7752b6dcf78d280ebde50b818ea
V <lytranvinh.work@gmail.com>
2024-07-10 01:57:00 +0700

    [Upgrade]: display devices cmp

--- utils/displayDevices.py
+++ utils/displayDevices.py
@@ -1,20 +1,38 @@
-import speech_recognition as sr
+import pyaudio
 
-# Initialize recognizer class
-recognizer = sr.Recognizer()
+# Initialize PyAudio
+pa = pyaudio.PyAudio()
 
-# Get list of all audio input devices
-all_microphones = sr.Microphone.list_microphone_names()
+# Print general information
+print("General Information:")
+print(f"Default Input Device Index: {pa.get_default_input_device_info()['index']}")
+print(f"Default Output Device Index: {pa.get_default_output_device_info()['index']}")
+print()
 
-# Print all available audio input devices
-print("Available Microphones:")
-for index, microphone_name in enumerate(all_microphones):
-    print(f"{index}: {microphone_name}")
+# Print detailed information about input devices (microphones)
+print("Input Devices (Microphones):")
+for i in range(pa.get_device_count()):
+    device_info = pa.get_device_info_by_index(i)
+    if device_info["maxInputChannels"] > 0:
+        print(f"Device {i}: {device_info['name']}")
+        print(f"  Channels: {device_info['maxInputChannels']}")
+        print(f"  Sample Rate: {int(device_info['defaultSampleRate'])}")
+        if 'inputLatency' in device_info:
+            print(f"  Input Latency: {device_info['inputLatency']}")
+        else:
+            print("  Input Latency: Not available")
+        print()
 
-# Get list of all audio output devices (if supported by the library)
-all_speakers = sr.AudioFile.list_devices()
-
-# Print all available audio output devices
-print("\nAvailable Speakers:")
-for index, speaker_name in enumerate(all_speakers):
-    print(f"{index}: {speaker_name}")
+# Print detailed information about output devices (speakers)
+print("Output Devices (Speakers):")
+for i in range(pa.get_device_count()):
+    device_info = pa.get_device_info_by_index(i)
+    if device_info["maxOutputChannels"] > 0:
+        print(f"Device {i}: {device_info['name']}")
+        print(f"  Channels: {device_info['maxOutputChannels']}")
+        print(f"  Sample Rate: {int(device_info['defaultSampleRate'])}")
+        if 'outputLatency' in device_info:
+            print(f"  Output Latency: {device_info['outputLatency']}")
+        else:
+            print("  Output Latency: Not available")
+        print()
